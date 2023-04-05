01eedf14022ed7855a890300e76cb0543c523a52
david <david.erving@hotmail.com>
2023-04-06 09:34:58 +1000

    replace urllib with requests

--- observations_polling.py
+++ observations_polling.py
@@ -1,14 +1,15 @@
 
 
-from urllib.request import Request, urlopen
-from urllib.error import URLError, HTTPError
-import json
+#from urllib.request import Request, urlopen
+#from urllib.error import URLError, HTTPError
+#import json
+import requests
 import datetime
 import time
 import paho.mqtt.client as mqtt
 
 # Set variables
-topic = "home/outside/sensor"
+topic = "home/outside/sensortest"
 measurementName = "temperature"
 location = "outside"
 
@@ -30,14 +31,18 @@ client.connect(broker_address, keepalive=500)
 
 while True:
 
-    try:
+#    try:
 
-        with urlopen(url) as response: 
-            jsonString = response.read()
-            jsonData = json.loads(jsonString.decode('utf-8'))
-            current_reading = jsonData['observations']['data'][0]
+#       with urlopen(url) as response: 
+    with requests.get(url) as response: 
 
-    except HTTPError as e:
+#           jsonString = response.read()
+#           jsonData = json.loads(jsonString.decode('utf-8'))
+        jsonData = response.json()
+
+        current_reading = jsonData['observations']['data'][0]
+
+"""     except HTTPError as e:
         print('The server couldn\'t fulfill the request.')
         print('Error code: ', e.code)
     
@@ -45,17 +50,17 @@ while True:
         print('We failed to reach a server.')
         print('Reason: ', e.reason)
     
-    else:
+    else: """
     
-        locals().update(current_reading)
+locals().update(current_reading)
 
-        reading_ts = datetime.datetime.strptime(aifstime_utc, '%Y%m%d%H%M%S')
-        reading_age = (datetime.datetime.utcnow() - reading_ts).seconds
+reading_ts = datetime.datetime.strptime(aifstime_utc, '%Y%m%d%H%M%S')
+reading_age = (datetime.datetime.utcnow() - reading_ts).seconds
 
-        dict_msg={"location":location,"temperature":air_temp, "feels_like":apparent_t, "humidity":rel_hum}
-        
-        msg = json.dumps(dict_msg)
+dict_msg={"location":location,"temperature":air_temp, "feels_like":apparent_t, "humidity":rel_hum}
 
-        client.publish(topic,msg)
+msg = json.dumps(dict_msg)
 
-    time.sleep(600)
+client.publish(topic,msg)
+
+time.sleep(600)
